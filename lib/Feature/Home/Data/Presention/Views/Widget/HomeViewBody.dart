369c172eb075f1b9535287c5d8d432cd55c6c5e6
abdelrhmanyousef <abdo.yusiif@gmail.com>
2023-08-05 08:03:00 +0200

    Create ListView to bestseller box with CustomScrol

--- lib/Feature/Home/Data/Presention/Views/Widget/HomeViewBody.dart
+++ lib/Feature/Home/Data/Presention/Views/Widget/HomeViewBody.dart
@@ -2,34 +2,47 @@ import 'package:booklyapp/Feature/Home/Data/Presention/Views/Widget/BookListView
 import 'package:booklyapp/core/utiles/Styless.dart';
 import 'package:flutter/material.dart';
 
-import 'BestSellerListView.dart';
 import 'CustomAppBar.dart';
+import 'ListViewBooksbestseller.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CustomAppBar(),
-          BookListView(),
-          SizedBox(
-            height: 45,
+    return const CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: CustomAppBar(),
+            ),
+            BookListView(),
+            SizedBox(
+              height: 45,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: Text(
+                "Best Seller",
+                style: Styels.textStyel18,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+          ],
+        )),
+        SliverFillRemaining(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: ListViewBooksbestseller(),
           ),
-          Text(
-            "Best Seller",
-            style: Styels.textStyel18,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          BestSellerListViewItems()
-        ],
-      ),
+        )
+      ],
     );
   }
 }
