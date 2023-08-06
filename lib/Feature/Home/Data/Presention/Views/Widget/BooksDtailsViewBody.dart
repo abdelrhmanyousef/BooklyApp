fc2b71c2be5ade78fa8b0681c918a485a370d946
abdelrhmanyousef <abdo.yusiif@gmail.com>
2023-08-06 06:32:33 +0200

    end

--- lib/Feature/Home/Data/Presention/Views/Widget/BooksDtailsViewBody.dart
+++ lib/Feature/Home/Data/Presention/Views/Widget/BooksDtailsViewBody.dart
@@ -1,76 +1,30 @@
-import 'package:booklyapp/Feature/Home/Data/Presention/Views/Widget/CustomListViewItem.dart';
-import 'package:booklyapp/core/utiles/Styless.dart';
 import 'package:flutter/material.dart';
 
-import '../../../../../../constans.dart';
-import 'BookRate.dart';
-import 'BoxAction.dart';
+import 'BooksDetailsection.dart';
 import 'CustomAppBarBookDetailsView.dart';
-import 'SimilarBoxListView.dart';
+import 'SimilarBoxSction.dart';
 
 class BooksDetailsViewBody extends StatelessWidget {
   const BooksDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
-    return CustomScrollView(
+    return const CustomScrollView(
       slivers: [
         SliverFillRemaining(
           child: SafeArea(
             child: Padding(
-              padding: const EdgeInsets.only(left: 30, right: 30, top: 10),
+              padding: EdgeInsets.only(left: 30, right: 30, top: 10),
               child: Column(
                 children: [
                   CustomAppBarBookDetailsView(),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: width * .25),
-                    child: ListViewItems(),
-                  ),
-                  const SizedBox(
-                    height: 43,
-                  ),
-                  Text(
-                    "The Jungle Book",
-                    style: Styels.textStyel20.copyWith(
-                      fontFamily: CinzelDecorative,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 4,
-                  ),
-                  Text(
-                    "Rudyard Kipling",
-                    style: Styels.textStyel16.copyWith(
-                        color: const Color.fromARGB(179, 221, 216, 216),
-                        fontWeight: FontWeight.w600),
-                  ),
-                  const SizedBox(
-                    height: 18,
-                  ),
-                  const BookRate(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                  ),
-                  const SizedBox(
-                    height: 37,
-                  ),
-                  const BookAction(),
-                  const Expanded(
+                  BooksDetailsection(),
+                  Expanded(
                     child: SizedBox(
                       height: 49,
                     ),
                   ),
-                  const Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      "You can also like",
-                      style: Styels.textStyel16,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  const SimilarBoxListView()
+                  SimilarBoxSction()
                 ],
               ),
             ),
