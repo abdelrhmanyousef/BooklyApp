27016562226c6d0fd6ea3e20e3e136eebbec405d
abdelrhmanyousef <abdo.yusiif@gmail.com>
2023-08-06 02:08:41 +0200

    Create image Book in detailswidget

--- lib/Feature/Home/Data/Presention/Views/Widget/BooksDtailsViewBody.dart
+++ lib/Feature/Home/Data/Presention/Views/Widget/BooksDtailsViewBody.dart
@@ -1,3 +1,4 @@
+import 'package:booklyapp/Feature/Home/Data/Presention/Views/Widget/CustomListViewItem.dart';
 import 'package:flutter/material.dart';
 
 import 'CustomAppBarBookDetailsView.dart';
@@ -7,12 +8,17 @@ class BooksDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.only(left: 30, right: 30, top: 10),
         child: Column(
           children: [
             CustomAppBarBookDetailsView(),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: width * .25),
+              child: ListViewItems(),
+            ),
           ],
         ),
       ),
