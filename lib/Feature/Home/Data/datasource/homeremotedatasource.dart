6ebca585ef30d5737f88027bc377f2d2d2f2fee3
abdelrhmanyousef <abdo.yusiif@gmail.com>
2023-08-08 07:51:16 +0200

    cashNewsedbooks

--- lib/Feature/Home/Data/datasource/homeremotedatasource.dart
+++ lib/Feature/Home/Data/datasource/homeremotedatasource.dart
@@ -27,15 +27,18 @@ class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
   Future<List<BookEntites>> fetchNewistBook() async {
     var data = await apiService.get(
         endPoint: "volumes?Filtering=free-ebooks&Sorting=newest&q=programing");
+    List<BookEntites> book = GetBooksList(data);
+    SaveDataBox(book, KNewsedBox);
+
     return GetBooksList(data);
   }
 
   List<BookEntites> GetBooksList(Map<String, dynamic> data) {
-    List<BookEntites> Books = [];
+    List<BookEntites> book = [];
     for (var bookMap in data['items']) {
-      Books.add(BookModel.fromJson(bookMap));
+      book.add(BookModel.fromJson(bookMap));
     }
-    SaveDataBox(Books, KBox);
-    return Books;
+    SaveDataBox(book, KBox);
+    return book;
   }
 }
