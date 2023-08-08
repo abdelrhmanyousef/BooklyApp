f0fbd9f8a78c4fc2bd86ce98b2d71f246dc89150
abdelrhmanyousef <abdo.yusiif@gmail.com>
2023-08-08 06:44:34 +0200

    crateHomeLocal DataSource

---
+++ lib/Feature/Home/Data/datasource/homeLocalDataSource.dart
@@ -0,0 +1,20 @@
+import '../../domain/entites/Homeentite.dart';
+
+abstract class HomeLocalDataSource {
+  List<BookEntites> fetchFeatureBooks();
+  List<BookEntites> fetchNewistBook();
+}
+
+class HomeLocalDataSourceImpl extends HomeLocalDataSource {
+  @override
+  List<BookEntites> fetchFeatureBooks() {
+    // TODO: implement fetchFeatureBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  List<BookEntites> fetchNewistBook() {
+    // TODO: implement fetchNewistBook
+    throw UnimplementedError();
+  }
+}
